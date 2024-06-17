e3c642d659f95abbbf594e6a5940c8ece646e94a
namanswamy <namanswami3696@gmail.com>
2024-06-18 00:54:27 +0530

    First Commit

--- app/src/main/java/com/example/maidforyouapp/LoginActivity.java
+++ app/src/main/java/com/example/maidforyouapp/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.example.maidforyouapp;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +16,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class LoginActivity extends AppCompatActivity {
 
-    Button signupbutton;
+    Button signupbutton,gobutton,forgotpass;
+    ImageView logoimage;
+    TextView welcometitle,signintext;
+    TextInputLayout username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +37,28 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         signupbutton = findViewById(R.id.signupbutton);
+        gobutton = findViewById(R.id.gobutton);
+        logoimage = findViewById(R.id.logoimage);
+        welcometitle = findViewById(R.id.welcometitle);
+        signintext=findViewById(R.id.signintext);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
 
-        signupbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+
+        signupbutton.setOnClickListener((view)-> {
                 Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
-                startActivity(intent);
-            }
+
+                Pair[] pairs = new Pair[6];
+
+                pairs[0] = new Pair<View,String>(logoimage,"logo_image");
+                pairs[1] = new Pair<View,String>(welcometitle,"logo_text");
+                pairs[2] = new Pair<View,String>(username,"useranimation");
+                pairs[3] = new Pair<View,String>(password,"passwordanimation");
+                pairs[4] = new Pair<View,String>(gobutton,"goanim");
+                pairs[5] = new Pair<View,String>(signupbutton,"newuseranim");
+
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this,pairs);
+            startActivity(intent, options.toBundle());
         });
 
 
